9b5b17014274da27b2bdbe766b5a0e4f3429c793
WillahelmGSA <guilherme_gsa@live.com>
2019-08-01 17:50:33 -0300

    Arquivos sobre o jogo do quinto semestre.

---
+++ projeto/Scenes/Menuzao.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_BotaoInicio_pressed():
+	get_tree().change_scene("res://Scenes/Fase1.tscn")
+
+func _on_FecharJogo_pressed():
+	get_tree().quit()
+
+
+func _on_FECHARJOGO_pressed():
+	pass # Replace with function body.
